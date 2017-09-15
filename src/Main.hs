fb7d195249051e36ad3f4e84bf20d163cfc8d536
Edward Pond <edward.pond@springer.com>
2017-09-15 17:10:31 +0100

    a little bit further

--- src/Main.hs
+++ src/Main.hs
@@ -57,3 +57,36 @@ alreadyGuessed (Puzzle _ _ guessed) char = char `elem` guessed
 renderPuzzleChar :: Maybe Char -> Char
 renderPuzzleChar Nothing = '_'
 renderPuzzleChar (Just char) = char
+
+fillInCharacter :: Puzzle -> Char -> Puzzle
+fillInCharacter (Puzzle word filledInSoFar guesses) c = Puzzle word newFilledInSoFar (c : guesses)
+    where zipper guessed wordChar guessChar =
+            if wordChar == guessed then Just wordChar else guessChar
+          newFilledInSoFar = zipWith (zipper c) word filledInSoFar
+
+handleGuess :: Puzzle -> Char -> IO Puzzle
+handleGuess puzzle guess = do
+    putStrLn $ "Your guess was: " ++ [guess]
+    case (charInWord puzzle guess, alreadyGuessed puzzle guess) of
+        (_, True) -> do
+            putStrLn "You already guessed that\
+                    \ character, pick \
+                    \ something else!"
+            return puzzle
+        (True, _) -> do
+            putStrLn "This character was in the\
+                    \ word, filling in the word\
+                    \ accordingly"
+            return (fillInCharacter puzzle guess)
+        (False, _) -> do
+            putStrLn "This character wasn't in\
+                    \ the word, try again."
+            return (fillInCharacter puzzle guess)
+
+gameOver :: Puzzle -> IO ()
+gameOver (Puzzle wordToGuess _ guessed) =
+    if (length guessed) > 7 then
+        do putStrLn "You lose!"
+           putStrLn $ "The word was: " ++ wordToGuess
+           exitSuccess
+    else return ()
